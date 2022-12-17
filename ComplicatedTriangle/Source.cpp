646288e8dcdfe9eb4a0c1093da0e452839cc47b2
JoeFrumenti <joefrumenti@gmail.com>
2022-12-16 18:36:01 -0800

    Moved second triangle to new VAO, changed second triangle's fragment shader

--- ComplicatedTriangle/Source.cpp
+++ ComplicatedTriangle/Source.cpp
@@ -20,24 +20,46 @@ const GLchar* fragmentShaderSource = "#version 330 core\n"
 "{\n"
 "\nFragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\0";
+
+const GLchar* fragmentShaderSource2 = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"\nFragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+"}\0";
+
+
+
+
 GLuint fragmentShader;
+GLuint fragmentShader2;
 
 GLuint shaderProgram;
-GLuint VAO;
+GLuint shaderProgram2;
 
+GLuint VAO;
+GLuint VBO;
+
+GLuint VAO2;
+GLuint VBO2;
 
 //vertices to make a triangle hopefully
 float vertices[] = { -1.0f, -0.5f, 0.0f,
 					  0.0f, -0.5f, 0.0f,
-					 -0.5f,  0.5f, 0.0f,
-					  0.0f, -0.5f, 0.0f,
-					  1.0f, -0.5f, 0.0f,
-					  0.5f,  0.5f, 0.0f
+					 -0.5f,  0.5f, 0.0f
+					  
 				      
 };
 
-//Vertex buffer object
-GLuint VBO;
+float vertices2[] = {
+					  0.0f, -0.5f, 0.0f,
+					  1.0f, -0.5f, 0.0f,
+					  0.5f,  0.5f, 0.0f
+
+};
+
+
+
 
 //resize window function
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -113,6 +135,10 @@ int main()
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
+	fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
+	glCompileShader(fragmentShader2);
+
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 
 	if (!success)
@@ -128,11 +154,16 @@ int main()
 
 	//make shaderProgram be a shader program object
 	shaderProgram = glCreateProgram();
+	shaderProgram2 = glCreateProgram();
 
 	//attach and link shaders to shader program
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
+
+	glAttachShader(shaderProgram2, vertexShader);
+	glAttachShader(shaderProgram2, fragmentShader2);
+	glLinkProgram(shaderProgram2);
 
 	//make sure it worked
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
@@ -159,9 +190,9 @@ int main()
 	glGenBuffers(1, &VBO);
 
 
+
 	//Bind all the buffers to the right buffer objects
 	glBindVertexArray(VAO);
-	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	//tell opengl how to read the buffers
@@ -176,6 +207,31 @@ int main()
 	glBindVertexArray(0);
 
 
+
+	//Do it again with the second buffer
+	glGenVertexArrays(1, &VAO2);
+	glGenBuffers(1, &VBO2);
+
+
+
+	//Bind all the buffers to the right buffer objects
+	glBindVertexArray(VAO2);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+
+	//tell opengl how to read the buffers
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	//set attribute array to the 1 position
+	glEnableVertexAttribArray(0);
+
+	// unbind buffer (everything has been set up in VAO2)
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+
+
 	//RENDER LOOP
 	while (!glfwWindowShouldClose(window))
 	{
@@ -187,11 +243,19 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);	//apply glclearcolor
 
 
-		//draw triangle
+		//Set orange shader
 		glUseProgram(shaderProgram);
+
+		//set VAO to first triangle and draw it
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawArrays(GL_TRIANGLES, 3, 6);
+
+		//set yellow shader
+		glUseProgram(shaderProgram2);
+
+		//set VAO to seconf triangle and draw it
+		glBindVertexArray(VAO2);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//check and call events and swap buffers
 		glfwSwapBuffers(window);
